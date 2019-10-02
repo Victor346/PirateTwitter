2ee424920bb993b1c2ae570309085c37c5f20cce
feriosch <47795404+feriosch@users.noreply.github.com>
2019-10-02 14:30:40 -0500

    Lo que hizo el vic

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var redis = require('redis'), client = redis.createClient();
+
+client.on("error", function (err) {
+  console.log("Error " + err);
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Pirate Twitter' });
 });
 
+router.post('/login', function(req, res, next){
+  res.render('error', { title: 'Errorrrr.'});
+});
+ 
+router.post('/signup', function(req, res, next){
+  let objetoDatos = {'username': req.body.username, 'password': req.body.password, 
+    'followers': 'listFollow' + req.body.username, 'following': 'listFollowing' + 
+    req.body.password};
+  let stringDatos = JSON.password(objetoDatos);
+
+  console.log(stringDatos);
+  res.render('signup', {username: stringDatos , password: req.form.password});
+});
 module.exports = router;
