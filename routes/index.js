e8eb425ba67bc122f4628c890d1168395a694aff
feriosch <47795404+feriosch@users.noreply.github.com>
2019-10-07 17:04:27 -0500

    prueba canales

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
-var redis = require('redis'), client = redis.createClient();
+var redis = require('redis'), client2 = redis.createClient(), client = redis.createClient()
 
 client.on("error", function (err) {
   console.log("Error " + err);
 });
+
+lista = ["fsdabhkfas", "asjkfg", "Mi canal favorito", "TJosdfpjdf", "FASFSD"]
+
+client2.on("message", function (channel, message) {
+  console.log("sub channel " + channel + ": " + message);
+});
+
+client2.subscribe(lista);
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,7 +21,37 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/login', function(req, res, next){
-  res.render('error', { title: 'Errorrrr.'});
+  let username = req.body.loginUsername
+  console.log('Username from post:' + username)
+  let password = req.body.loginPassword;
+  client.publish('Mi canal favorito', 'Mi mama me mima');
+  client.publish('fsdabhkfas', 'Mi mama me mima');
+  client.publish('asjkfg', 'Mi mama me mima');
+  client.publish('TJosdfpjdf', 'Mi mama me mima');
+  client.publish('FASFSD', 'Mi mama me mima');
+  client.hget('users', username, function(err, result){
+    if(result === null){
+      res.render('error', { title: "No existe dicho usuario" });
+      next();
+    } else {
+      userJSON = JSON.parse(result);
+      console.log(userJSON);
+      if(password === userJSON.password){
+        res.render('login', { username: userJSON.password });
+      } else {
+        res.render('error', { title: "Contraseña incorrecta" });
+      }
+      
+    }
+  });
+  /*console.log(userRedis);
+  if (userRedis === null) {
+    res.render('error', { title: "No existe dicho usuario" });
+    next();
+  } else {
+    res.render('login', { username: userRedis });
+  }*/
+  //res.render('login', { username: "Hola" });
 });
  
 router.post('/signup', function(req, res, next){
@@ -28,9 +67,9 @@ router.post('/signup', function(req, res, next){
       client.lpush(objetoDatos.following, username);
       client.lpush(objetoDatos.followers, username);
       client.lpush(objetoDatos.timeline, 'Me uni a Pirate Twitter - Vic');
-      res.render('signup', {Mensaje: "Felicidades te registraste" , Payload: stringDatos});
+      res.render('signup', {Mensaje: "Felicidades te registraste." , Payload: stringDatos});
     } else {
-      res.render('signup', {Mensaje: "Lo siento amigo" , Payload: stringDatos});
+      res.render('signup', {Mensaje: "Usuario ya registrado." , Payload: stringDatos});
     }
  
   });
