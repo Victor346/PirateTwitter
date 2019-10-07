bb2a3590df3eee21e9874b3ee626095e25cfb8d9
InJoong <injoong01@hotmail.com>
2019-10-07 17:54:23 -0500

    Commit

---
+++ routes/homepage.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var redis = require('redis'), client = redis.createClient(), client2 = redis.createClient();
+
+router.get('', function(req, res, next) {
+    
+});
+
+router.post('/follow', function(req, res, next) {
+    let sender = req.body.sender;
+    let receiver = re.body.receiver;
+
+    client.lpush('listFollowing' + receiver, sender);
+    client.lpush('listFollowers' + sender, receiver);
+
+    client2.unsubscribe();
+
+    client.lrange('listFollowing' + receiver, 0, -1, function(err, reply){
+        client.subscribe(reply);
+    });
+
+    res.render();
+});
+
+router.post('/publish', function(req, res, next) {
+    let username = req.body.username;
+    let message = req.body.message;
+
+    client.publish(username, message);
+    
+    res.render();
+});
--- routes/login.js
+++ routes/login.js
@@ -2,17 +2,6 @@ var express = require('express');
 var router = express.Router();
 var redis = require('redis'), client = redis.createClient();
 
-router.post('/follow', function(req, res, next) {
-    let username = req.body.username;
-    let 
-});
-
-router.get('/follow', function(req, res, next) {
-    
-});
-
-router.post('/publish', function(req, res, next) {
-    let username = req.body.username;
-    let message = req.body.message;
+router.post('', function(req, res, next) {
     
 });
